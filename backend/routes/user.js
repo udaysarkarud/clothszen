3266e01661c2679761c8e4e90bf6d719e2650c03
udaysarkarud <udaysarkarud@gmail.com>
2022-06-15 23:00:04 +0600

    jwt varification done

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,23 @@
 import express from 'express';
 const router = express.Router();
+import jwt from "jsonwebtoken";
+import User from '../models/User.js';
+import verifyTokenAndAuthorization from './verifyToken.js';
 
-router.get('/:id',(req,res)=>{
-    res.send('user test is successfull')
+//UPDATE
+router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
+    if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString()
+    }
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, { new: true })
+        res.status(200).json(updatedUser);
+    } catch(err){
+        res.status(200).json(err)
+    }
 })
 
 export default router;
