8d864773eb02052906e2826ca64281748f99aa50
udaysarkarud <udaysarkarud@gmail.com>
2022-06-16 19:33:21 +0600

    added products api with add, delete, get and get all

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -53,30 +53,24 @@ router.get('/find/:id', async (req, res) => {
 //GET ALL PRODUCT
 router.get('/', async (req, res) => {
     const qNew = req.query.new
-    const qCategory = req.query.new
+    const qCategory = req.query.category
     try {
-        const products = query ? await Product.find().sort({ _id: -1 }).limit(5) : await Product.find()
+        let products;
+        if (qNew) {
+            products = await Product.find().sort({ createdAt: -1 }).limit(1);
+        } else if (qCategory) {
+            products = await Product.find({
+                categories: {
+                    $in: [qCategory],
+                }
+            });
+        } else {
+            products = await Product.find();
+        }
         res.status(200).json(products)
     } catch (err) {
         res.status(500).json(err);
     }
 })
-
-// //GET USER STATS
-// router.get('/stats', verifyTokenAndAdmin, async (req, res) => {
-//     const date = new Date();
-//     const lastYear = new Date(date.setFullYear(date.getFullYear() - 1))
-
-//     try {
-//         const data = await User.aggregate([
-//             { $match: { createdAt: { $gte: lastYear } } },
-//             {$project: {month:{$month:'$createdAt'}}},
-//             {$group:{_id:'$month',total:{$sum:1}}}
-//         ])
-//         res.status(200).json(data)
-//     } catch (err) {
-//         res.status(500).json(err)
-//     }
-// })
 
 export default router;
